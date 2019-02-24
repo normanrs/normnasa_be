13d8ea82bdd151508c8f4a3256aedd7e90fead9b
normanrs <normanrs@live.com>
2019-02-23 18:18:48 -0700

    Added twitter gem setup ENV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  require 'twitter'
+
+  private
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['tw_apikey']
+      config.consumer_secret     = ENV['tw_apisecret']
+      config.access_token        = ENV['tw_acctoken']
+      config.access_token_secret = ENV['tw_accsecret']
+    end
+
 end
